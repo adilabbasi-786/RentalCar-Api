c0a2b7f5f6cc9f208388d7dd3e88bbd5cbb25228
adilabbasi-786 <m.adilabbasi786@gmail.com>
2023-07-04 22:07:26 +0500

    initial backup Setup

---
+++ src/api/crousel/controllers/crousel.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * crousel controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::crousel.crousel');
---
+++ src/api/crousel/services/crousel.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * crousel service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::crousel.crousel');
