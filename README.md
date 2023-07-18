beefcc0a2067e4353f25aed4e1cbffbf674f043b
adilabbasi-786 <m.adilabbasi786@gmail.com>
2023-07-18 21:12:30 +0500

    backend form initial

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+npm run strapi admin:reset-user-password --email="m.adilabbasi786@gmail.com" --password="\*RUFStsTd62mUr@"
+email: m.adilabbasi786@gmail.com
+password:\*RUFStsTd62mUr@
+
 # RentalCar-Api
--- src/api/booking/controllers/booking.js
+++ src/api/booking/controllers/booking.js
@@ -1,9 +1,35 @@
-'use strict';
+"use strict";
 
 /**
  * booking controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::booking.booking');
+module.exports = createCoreController("api::booking.booking", ({ strapi }) => ({
+  async find(ctx) {
+    // some custom logic here
+    ctx.query = { ...ctx.query, local: "en" };
+
+    // Calling the default core action
+    const userId = ctx.state.user.id;
+    const entries = await strapi.db.query("api::booking.booking").findMany({
+      select: ["name"],
+      where: { users_permissions_user: userId },
+    });
+    console.log(entries);
+    // const { data, meta } = await super.find(ctx);
+
+    // some more custom logic
+    // meta.date = Date.now();
+
+    return { data: entries };
+  },
+  async create(ctx) {
+    ctx.request.body.data.users_permissions_user = ctx.state.user.id;
+    let entity = await strapi
+      .service("api::booking.booking")
+      .create(ctx.request.body);
+    return entity;
+  },
+}));
